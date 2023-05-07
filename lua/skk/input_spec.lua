81abbc1783aedc0993d7ec093bbcc4ef748866d9
uga-rosa <uga6603@gmail.com>
2023-05-07 13:52:36 +0900

    test(input): refactor

--- lua/skk/input_spec.lua
+++ lua/skk/input_spec.lua
@@ -4,36 +4,33 @@ local Context = require("skk.context")
 ---@type Context
 local context
 
+---@param input string
+---@param expect string
+local function test(input, expect)
+  for char in vim.gsplit(input, "") do
+    Input.kanaInput(context, char)
+  end
+  assert.are.equals(expect, context.fixed)
+end
+
 describe("Tests for input.lua", function()
   before_each(function()
     context = Context.new()
   end)
 
   it("single char", function()
-    for char in vim.gsplit("ka", "") do
-      Input.kanaInput(context, char)
-    end
-    assert.are.equals("か", context.fixed)
+    test("ka", "か")
   end)
 
   it("multiple chars (don't use tmpResult)", function()
-    for char in vim.gsplit("ohayou", "") do
-      Input.kanaInput(context, char)
-    end
-    assert.are.equals("おはよう", context.fixed)
+    test("ohayou", "おはよう")
   end)
 
   it("multiple chars (use tmpResult)", function()
-    for char in vim.gsplit("amenbo", "") do
-      Input.kanaInput(context, char)
-    end
-    assert.are.equals("あめんぼ", context.fixed)
+    test("amenbo", "あめんぼ")
   end)
 
   it("multiple chars (use tmpResult and its next)", function()
-    for char in vim.gsplit("uwwwa", "") do
-      Input.kanaInput(context, char)
-    end
-    assert.are.equals("うwっわ", context.fixed)
+    test("uwwwa", "うwっわ")
   end)
 end)
