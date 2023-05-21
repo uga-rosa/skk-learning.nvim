c88cfba4d2ce62c5fbc7216d3223a05affe6668a
uga-rosa <uga6603@gmail.com>
2023-05-21 19:52:56 +0900

    Update context.lua

--- lua/skk/context.lua
+++ lua/skk/context.lua
@@ -1,30 +1,37 @@
 local KanaTable = require("skk.kana.kana_table")
+local PreEdit = require("skk.preedit")
+local InputState = require("skk.state").InputState
 
 ---@class Context
 ---@field kanaTable KanaTable 全ての変換ルール
----@field fixed string もう確定したひらがな
----@field feed string 未確定のローマ字
+---@field preEdit PreEdit
+---@field state State
 ---@field tmpResult? KanaRule feedに完全一致する変換ルール
 local Context = {}
 
 function Context.new()
   local self = setmetatable({}, { __index = Context })
   self.kanaTable = KanaTable.new()
-  self.fixed = ""
-  self.feed = ""
+  self.preEdit = PreEdit.new()
+  self.state = InputState.new()
   return self
 end
 
 ---@param candidates? KanaRule[]
 function Context:updateTmpResult(candidates)
-  candidates = candidates or self.kanaTable:filter(self.feed)
+  candidates = candidates or self.kanaTable:filter(self.state.feed)
   self.tmpResult = nil
   for _, candidate in ipairs(candidates) do
-    if candidate.input == self.feed then
+    if candidate.input == self.state.feed then
       self.tmpResult = candidate
       break
     end
   end
+end
+
+---@return string
+function Context:toString()
+  return self.state:toString()
 end
 
 return Context
