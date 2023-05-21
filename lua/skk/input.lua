7ba357c98e780d158bb3379ae807a60f0549b37e
uga-rosa <uga6603@gmail.com>
2023-05-22 01:05:30 +0900

    Update input.lua

--- lua/skk/input.lua
+++ lua/skk/input.lua
@@ -1,28 +1,38 @@
 local Input = {}
 
 ---@param context Context
+---@param result KanaRule
+local function acceptResult(context, result)
+  local preEdit = context.preEdit
+  local state = context.state
+  local kana, feed = result.output, result.next
+
+  preEdit:doKakutei(kana)
+  state.feed = feed
+end
+
+---@param context Context
 ---@param char string
 function Input.kanaInput(context, char)
-  local input = context.feed .. char
+  local state = context.state
+  local input = state.feed .. char
   local candidates = context.kanaTable:filter(input)
   if #candidates == 1 and candidates[1].input == input then
     -- 候補が一つかつ完全一致。確定
-    context.fixed = context.fixed .. candidates[1].output
-    context.feed = candidates[1].next
+    acceptResult(context, candidates[1])
     context:updateTmpResult()
   elseif #candidates > 0 then
     -- 未確定
-    context.feed = input
+    state.feed = input
     context:updateTmpResult(candidates)
   elseif context.tmpResult then
     -- 新しい入力によりtmpResultで確定
-    context.fixed = context.fixed .. context.tmpResult.output
-    context.feed = context.tmpResult.next
+    acceptResult(context, context.tmpResult)
     context:updateTmpResult()
     Input.kanaInput(context, char)
   else
     -- 入力ミス。context.tmpResultは既にnil
-    context.feed = ""
+    state.feed = ""
     Input.kanaInput(context, char)
   end
 end
